706abf35c06a945b41c6d1ba08868fca3fca4db6
marko <mailme@markokrstic.com>
2021-01-05 14:19:06 +0100

    added php files

---
+++ dp_framework.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name:       DP Framework
+ * Plugin URI:        https://dolugins.com
+ * Description:       Handle the basics with this plugin.
+ * Version:           0.0.1
+ * Author:            dplugins
