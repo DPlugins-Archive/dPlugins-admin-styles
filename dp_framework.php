b73e7461d2cc8b3b68261730dba0de77cec9bc7e
cmsExpertss <63702033+cmsExpertss@users.noreply.github.com>
2021-01-05 18:33:52 +0500

    Plugin and included files

--- dp_framework.php
+++ dp_framework.php
@@ -8,6 +8,28 @@
  * Author:            dplugins
 **/
 
+define('DP_URL',	plugin_dir_url(__FILE__));
 
+function DP_settings_page() {
+	add_menu_page( 
+		'DP Framework', 
+		'DP Framework', 
+		'manage_options', 
+		'dp-framework', 
+		'DP_framework_page',
+		'dashicons-editor-code'
+	);
 
-// require_once 'styleguide.php';
+	add_submenu_page( SCORG_SAMPLE_PLUGIN_LICENSE_PAGE, 'License', 'License', 'manage_options', SCORG_SAMPLE_PLUGIN_LICENSE_PAGE, 'SCORG_license_page', 9999 );
+}
+add_action('admin_menu', 'DP_settings_page');
+
+function DP_framework_page(){
+	require_once 'styleguide.php';
+}
+
+function DP_enqueue_admin_script() {
+	wp_enqueue_style( 'DP_admin_css', DP_URL . 'css/admin.css');
+}
+
+add_action( 'admin_enqueue_scripts', 'DP_enqueue_admin_script' );
