51f419a871294f0d4f9ae6e0c133ddeb5ca4f26c
Robert Spectrum <robospectrum@gmail.com>
2022-05-21 11:48:44 +0300

    1.0.1

--- dp_framework.php
+++ dp_framework.php
@@ -4,7 +4,7 @@
  * Plugin Name: DP Framework
  * Plugin URI: https://dolugins.com/
  * Description: Handle the basics with this plugin.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: dplugins
  * Author URI: https://dolugins.com/
 */
